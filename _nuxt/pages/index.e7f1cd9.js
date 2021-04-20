d4bb6e8120aa178415f574b3d7361fb05bd20f97
Enmaku <enmaku@protonmail.com>
2021-04-20 19:10:53 +0000

    1.0.0

---
+++ _nuxt/pages/index.e7f1cd9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{942:function(t,e,n){"use strict";n.r(e);var c={layout:"default",fetch:function(t){(0,t.redirect)("/deposit")}},l=n(17),component=Object(l.a)(c,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
